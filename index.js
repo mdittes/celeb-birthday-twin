472c6a448db925d7937ca3c82c32ed31559d9fa0
Matt Dittes <mattdittes@gmail.com>
2022-08-02 14:49:11 -0400

    Attempt at fnxs working

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const options = {
 	}
 };
 
+const baseUrl = 'https://online.movie-database.p.rapidapi.com/actors/'
+//const formUrl = `${baseUrl}list-born-today?month=${monthNum}&day=${dayNum}`
+//const hoverUrl = `${baseUrl}get-known-for?nconst=${actorName}`
+//const bdayUrl = `${baseUrl}${renderBirthdays}`
+
 //fetch for pulling birthdays on that day - will need ${} to obtain new dates on page load (current date) and after user form submit
 fetch('https://online-movie-database.p.rapidapi.com/actors/list-born-today?month=12&day=21', options)
 	.then(response => response.json())
@@ -24,8 +29,19 @@ fetch('https://online-movie-database.p.rapidapi.com/actors/get-known-for?nconst=
 const birthdayContainers = document.querySelector('#birthday-cards')
 	
 const renderBirthdays = function(actor) {
-	 actor.slice(0,4).forEach(actor => {
-		console.log(actor)
+	 actor.slice(0,4).forEach((actor, index) => {
+		fetchBio(actor)
 	})
 }
 // need actor.slice(0,4) to only iterate over first 4 elements returned in fetch
+
+//need fnx to grab name (and image) from the array of bdays that day from API
+const fetchBio = function(actor) {
+	fetch(`${baseUrl}get-bio?nconst=${actor}`, options)
+	.then(res => res.json())
+	.then(console.log(actor))
+}
+
+fetch('https://online-movie-database.p.rapidapi.com/actors/get-bio?nconst=nm0836432', options)
+.then(res => res.json())
+.then(data => console.log(data))
