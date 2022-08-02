764f0cb5d9630d6635c1d2d3aabe020dcb14f920
Matt Dittes <mattdittes@gmail.com>
2022-08-02 11:08:53 -0400

    Making progress on fnxs

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const options = {
 //fetch for pulling birthdays on that day - will need ${} to obtain new dates on page load (current date) and after user form submit
 fetch('https://online-movie-database.p.rapidapi.com/actors/list-born-today?month=12&day=21', options)
 	.then(response => response.json())
-	.then(response => console.log(response))
+	.then(actors => renderBirthdays(actors))
 	.catch(err => console.error(err));
 
 //fetch for pulling actor most known for project - again will need ${} to tie back to above fetch/user submit and populated actors so "nm#######" shows image/title for 
@@ -18,4 +18,13 @@ fetch('https://online-movie-database.p.rapidapi.com/actors/get-known-for?nconst=
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
 
+//need to adjust return of first fetch so that it only returns/populates the top 4 results	
+	//return will be the actor's name/imdb profile - will need to populate with both image associated with that account and name underneath (two separate blocks)
+
+const birthdayContainers = document.querySelector('#birthday-cards')
 	
+const renderBirthdays = function(actor) {
+	for(let i=0; i < actor[0]; i++) {
+		console.log(actor)
+	}
+}
